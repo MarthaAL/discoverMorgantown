b527564c6e8a6027d6c6c6901b431b5fc49702a4
test <test@gmail.com>
2021-03-07 17:03:34 -0500

    Sprint #1

--- login.php
+++ login.php
@@ -1,37 +1,31 @@
-<link href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+<?php
+require 'includes/header.php';
+?>
 
-<script src="http://mymaplist.com/js/vendor/TweenLite.min.js"></script>
-<!-- This is a very simple parallax effect achieved by simple CSS 3 multiple backgrounds, made by http://twitter.com/msurguy -->
+<main>
+    <link rel="stylesheet" href="css/login.css">
 
-<div class="container">
-    <div class="row vertical-offset-100">
-    	<div class="col-md-4 col-md-offset-4">
-    		<div class="panel panel-default">
-			  	<div class="panel-heading">
-			    	<h3 class="panel-title">Please sign in</h3>
-			 	</div>
-			  	<div class="panel-body">
-			    	<form accept-charset="UTF-8" role="form">
-                    <fieldset>
-			    	  	<div class="form-group">
-			    		    <input class="form-control" placeholder="E-mail" name="email" type="text">
-			    		</div>
-			    		<div class="form-group">
-			    			<input class="form-control" placeholder="Password" name="password" type="password" value="">
-			    		</div>
-			    		<div class="checkbox">
-			    	    	<label>
-			    	    		<input name="remember" type="checkbox" value="Remember Me"> Remember Me
-			    	    	</label>
-			    	    </div>
-			    		<input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
-			    	</fieldset>
-			      	</form>
-			    </div>
-			</div>
-		</div>
-	</div>
-</div>
+    <div class="bg-cover">
+        <div class="h-40 center-me">
+            <div class="my-auto">
+                <form class="form-signin" action="includes/login-helper.php" method="post">
+                    <h2>Login</h2>
+                    <p class="hint-text">Let's Discover Together</p>
+                    <div class="form-group">
+                        <input type="text" class="form-control" name="uname-email" placeholder="Username/ Email"
+                            required="required">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" name="pwd" placeholder="Password"
+                            required="required">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" name="login-submit" class="btn btn-lg btn-outline-warning btn-block">Sign
+                            In</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+</main>
