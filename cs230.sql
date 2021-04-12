2928cc2850345238273ff31d3190604f58f9c587
test <test@gmail.com>
2021-04-11 21:30:29 -0400

    adding sql file

---
+++ cs230.sql
@@ -0,0 +1,244 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Apr 11, 2021 at 09:25 PM
+-- Server version: 5.7.33-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cs230`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `night`
+--
+
+CREATE TABLE `night` (
+  `itemid` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `name` varchar(50) NOT NULL COMMENT 'restraunt name',
+  `location` varchar(50) NOT NULL,
+  `open` time NOT NULL COMMENT 'opening time',
+  `cost` int(11) NOT NULL COMMENT '$ - $$$; 1-3',
+  `tags` varchar(80) NOT NULL COMMENT 'keywords to search',
+  `close` time NOT NULL COMMENT 'closing time',
+  `pic` varchar(50) NOT NULL DEFAULT '../images/default.jpg',
+  `fakes` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `outdoor`
+--
+
+CREATE TABLE `outdoor` (
+  `itemid` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `open` time NOT NULL,
+  `close` time NOT NULL,
+  `tags` varchar(80) NOT NULL,
+  `pic` varchar(50) NOT NULL DEFAULT '../images/default.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profiles`
+--
+
+CREATE TABLE `profiles` (
+  `pid` int(11) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
+  `uname` varchar(50) DEFAULT NULL,
+  `profpic` varchar(50) NOT NULL DEFAULT '../images/default.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Dumping data for table `profiles`
+--
+
+INSERT INTO `profiles` (`pid`, `fname`, `uname`, `profpic`) VALUES
+(1, 'hsfetty', 'hsfetty', '../images/default.jpg'),
+(2, 'Delaney', 'dmirwin', '../images/default.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `restraunts`
+--
+
+CREATE TABLE `restraunts` (
+  `itemid` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `name` varchar(50) NOT NULL COMMENT 'restraunt name',
+  `location` varchar(50) NOT NULL,
+  `open` time NOT NULL COMMENT 'opening time',
+  `cost` int(11) NOT NULL COMMENT '$ - $$$; 1-3',
+  `tags` varchar(80) NOT NULL COMMENT 'keywords to search',
+  `close` time NOT NULL COMMENT 'closing time',
+  `pic` varchar(50) NOT NULL DEFAULT '../images/default.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `retail`
+--
+
+CREATE TABLE `retail` (
+  `itemid` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `name` varchar(50) NOT NULL COMMENT 'restraunt name',
+  `location` varchar(50) NOT NULL,
+  `open` time NOT NULL COMMENT 'opening time',
+  `cost` int(11) NOT NULL COMMENT '$ - $$$; 1-3',
+  `tags` varchar(80) NOT NULL COMMENT 'keywords to search',
+  `close` time NOT NULL COMMENT 'closing time',
+  `pic` varchar(50) NOT NULL DEFAULT '../images/default.jpg'
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `uid` int(11) NOT NULL,
+  `fname` varchar(50) DEFAULT NULL,
+  `lname` varchar(50) DEFAULT NULL,
+  `uname` varchar(50) DEFAULT NULL,
+  `password` varchar(120) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16 COMMENT='user database for signup and signin';
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`uid`, `fname`, `lname`, `uname`, `password`, `email`) VALUES
+(1, 'hsfetty', 'hsfetty', 'hsfetty', '$2y$10$Hk5fh4s/kluoHiWj46kF6eBElT6YQK27F8PvvsWLC628C4Src6Z/u', 'hs@fetty.com'),
+(2, 'Delaney', 'Irwin', 'dmirwin', '$2y$10$V/9YJ/v0W97k/SFjb6r/2OA8TG8wjf5QNdbNuFtNjd/Lp1zWFSkju', 'dmi0003@mix.wvu.edu');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `volunteer`
+--
+
+CREATE TABLE `volunteer` (
+  `itemid` int(11) NOT NULL,
+  `description` text NOT NULL,
+  `name` varchar(50) NOT NULL COMMENT 'restraunt name',
+  `location` varchar(50) NOT NULL,
+  `open` time NOT NULL COMMENT 'opening time',
+  `tags` varchar(80) NOT NULL COMMENT 'keywords to search',
+  `close` time NOT NULL COMMENT 'closing time',
+  `pic` varchar(50) NOT NULL DEFAULT '../images/default.jpg',
+  `contact` varchar(80) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf16;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `night`
+--
+ALTER TABLE `night`
+  ADD PRIMARY KEY (`itemid`);
+
+--
+-- Indexes for table `outdoor`
+--
+ALTER TABLE `outdoor`
+  ADD PRIMARY KEY (`itemid`);
+
+--
+-- Indexes for table `profiles`
+--
+ALTER TABLE `profiles`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `restraunts`
+--
+ALTER TABLE `restraunts`
+  ADD PRIMARY KEY (`itemid`);
+
+--
+-- Indexes for table `retail`
+--
+ALTER TABLE `retail`
+  ADD PRIMARY KEY (`itemid`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indexes for table `volunteer`
+--
+ALTER TABLE `volunteer`
+  ADD PRIMARY KEY (`itemid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `night`
+--
+ALTER TABLE `night`
+  MODIFY `itemid` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `outdoor`
+--
+ALTER TABLE `outdoor`
+  MODIFY `itemid` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `profiles`
+--
+ALTER TABLE `profiles`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `restraunts`
+--
+ALTER TABLE `restraunts`
+  MODIFY `itemid` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `retail`
+--
+ALTER TABLE `retail`
+  MODIFY `itemid` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `volunteer`
+--
+ALTER TABLE `volunteer`
+  MODIFY `itemid` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
