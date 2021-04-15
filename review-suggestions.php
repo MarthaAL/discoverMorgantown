b938b6e90a8ba7d8278c5f34ea8ff70be28ea57e
callynz <cmz0007@mix.wvu.edu>
2021-04-15 17:08:33 -0400

    test 1

---
+++ review-suggestions.php
@@ -0,0 +1,29 @@
+<?php
+require 'includes/header.php';
+?>
+
+<main>
+    <link rel="stylesheet" href="css/reviewsuggestions.css">
+
+    <h1>Review Suggestions</h1>
+    <div class="reviewsuggestions-container">
+
+        <?php
+            include_once 'includes/dbhandler.php';
+            $sql = "SELECT * FROM suggestions ORDER BY upload_date DESC";
+            $query = mysqli_query($conn, $sql);
+
+            while($row = mysqli_fetch_assoc($query)) {
+                echo '    <div class="card">
+                <a href="suggestions.php?id='.$row['sid'].'">
+                <h3>'.$row["name"].'</h3>
+                <p>'.$row["address"].'</p>
+                <p>'.$row["descript"].'</p>
+                </a>
+               </div>';
+            }
+        ?>
+    </div>
+
+
+</main>
