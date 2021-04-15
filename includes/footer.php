50d56107b998a8edba327f5d84de002a75e80631
akroyce25 <akr0030@mix.wvu.edu>
2021-04-14 21:01:39 -0400

    header and footer updated code

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer style="background-color: rgb(0, 40, 85); margin: 0; overflow: hidden; position: sticky; top: 0; padding: 0;">
+<ul style="margin: 0; padding: 0; display: flex; justify-content: center; white-space: nowrap; overflow-x: hidden; overflow-y: hidden; height: 50px; padding-right: 25px; padding-left: 25px; font-size: 25px;">
+    <a style="padding: 12.5px; color: rgb(234, 170, 0);">CS 230 Group 3 Project</a>
+</ul></footer>
+
--- includes/header.php
+++ includes/header.php
@@ -21,21 +21,17 @@ session_start();
     <link rel="stylesheet" href="../css/header.css">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"
-
         integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-
         integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
 
     </script>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
 
     </script>
@@ -47,161 +43,61 @@ session_start();
 </head>
 
 <header>
-
+    <div>
     <nav>
-
         <ul class="left">
-
             <li>
-
-
-
                 <p2 style="padding: 18px;">Discover Morgantown</p2>
-
-
-
             </li>
-
         </ul>
-
         <ul style="margin-top: -75px;">
-
             <?php if (isset($_SESSION['uid'])) {
 
                         echo '
-
-                        <li>
-
-                        <a href="../home.php">
-
-                        <p style="padding: 18px;">Home</p>
-
-                        </a>
-
-                        </li>
-
-                        <li>
-
-                        <a href="../activities.php">
-
-                        <p style="padding: 18px;">Activities</p>
-
-                        </a>
-
-                        </li>
-
-                        <li>
-
-                        <a href="../about.php">
-
-                        <p style="padding: 18px;">About</p>
-
-                        </a>
-
-                        </li>
-
-                        ';
-
+                        <li><a href="../home.php"><p style="padding: 18px;">Home</p></a></li>
+                        <li><a href="../activities.php"><p style="padding: 18px;">Activities</p></a></li>
+                        <li><a href="../about.php"><p style="padding: 18px;">About</p></a></li>';
                     }
 
                     else {
 
                         echo '
-
-                        <li>
-
-                    <a href="../home.php">
-
-                            <p style="padding: 18px;">Home</p>
-
-                        </a>
-
-                    </li>
-
-                    <li>
-
-                        <a href="../login.php">
-
-                        <p style="padding: 18px;">Login</p>
-
-                        </a>
-
-                    </li>
-
-                    <li>
-
-                    <a href="../signup.php">
-
-                    <p style="padding: 18px;">Signup</p>
-
-                    </a>
-
-                    </li>
-
-                    <li>
-
-                    <a href="../about.php">
-
-                            <p style="padding: 18px;">About</p>
-
-                        </a>
-
-                    </li>
-
-                    ';
-
+                        <li><a href="../home.php"><p style="padding: 18px;">Home</p></a></li>
+                        <li><a href="../login.php"><p style="padding: 18px;">Login</p></a></li>
+                        <li><a href="../signup.php"><p style="padding: 18px;">Signup</p></a></li>
+                        <li><a href="../about.php"><p style="padding: 18px;">About</p></a></li>';
                     }
-
-                    ?>
-
+                    ?>        
         </ul>
-
-        <ul class="right" style="margin-top: -75px;">
-
-        <?php if (isset($_SESSION['uid'])) {
+        </nav>
+        <ul class="right" style="margin-left: -100px; padding-left: 0px; margin-top: -75px;">
+            <?php if (isset($_SESSION['uid'])) {
 
                         echo '
-
-                        <li>
-
-                    <a href="includes/logout.php">
-
-                    <p style="padding: 18px;">Signout</p>
-
-                    </a>
-
-                    </li>
-
-                        <li>
-
-                <a class="profile circle" style="margin-left: 25px; margin-top: 8px; background-image: url(../images/login-background.png);" href="../profile.php">
-
-                </a>
-
-            </li>';
-
+                        <div class="dropdown circle" style="margin-left: 0px; margin-top: 8px;">
+                        <button class="profile circle" style="background-image: url(../images/default.png);"></button>
+                        <div class="dropdown-content" style="transform: translate(-45px, 0px); margin-right: 50px;">
+                        <a style="background-color: rgb(0, 40, 85); font-size: 0px; padding: 4px;"></a>
+                        <li><a class="dropdown-item" href="../profile.php">Profile</a></li>
+                        <li><a class="dropdown-item" href="../favorties.php">Favorites</a></li>
+                        <li><a class="dropdown-item" href="../logout.php">Signout</a></li>
+                        </div>
+                        </div>';
                     }
 
                     else {
 
                         echo '
-
-                        <li>
-
-                <a class="profile circle" style="margin-top: 8px; background-image: url(../images/default.png);" href="../login.php">
-
-                </a>
-
-            </li>
-
-                    ';
-
+                        <div class="dropdown circle" style="margin-left: 0px; margin-top: 8px;">
+                        <button class="profile circle" style="background-image: url(../images/default.png);"></button>
+                        <div class="dropdown-content" style="transform: translate(-76px, 0px);">
+                        <a style="background-color: rgb(0, 40, 85); font-size: 0px; padding: 4px;"></a>
+                        <li><a class="dropdown-item" href="../login.php">Login</a></li>
+                        <li><a class="dropdown-item" href="../signup.php">Signup</a></li>
+                        </div>
+                        </div>';
                     }
-
                     ?>
-
         </ul>
-
-    </nav>
-
+    </div>
 </header>
