f99aa62efdaf49314c4e6713168df9b9c3e09529
dmi0003 <70488950+dmi0003@users.noreply.github.com>
2021-04-19 16:58:04 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,108 +1,201 @@
-<?php
-require 'includes/header.php';
-?>
-
-<body>
-    <link rel="stylesheet" href="css/about.css">
-
-    <div class="about">
-        <h1>About Us</h1>
-        <p>We are Group 3!</p>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src=".JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c1">Alex Royce</p>
-                    <p>Email: </p>
+<div class="about-section">
+    <h1>About Us Page</h1>
+    <p>Some text about who we are and what we do.</p>
+    <p>Resize the browser window to see that this page is responsive by the way.</p>
+  </div>
+  
+    <?php
+    require 'includes/header.php';
+    ?>
+    
+    <body>
+        <link rel="stylesheet" href="css/about.css">
+    
+        <div class="about">
+            <h1>About Us</h1>
+            <p>We are Group 3!</p>
+        </div>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src=".JPG">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Alex Royce</p>
+                        <p>Email: </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src=".JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c2">Callyn Zeigler</p>
-                    <p>Email: </p>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src=".JPG">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Callyn Zeigler</p>
+                        <p>Email: </p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src="images/.JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c1">Harley Frazee</p>
-                    <p>Email:</p>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/.JPG">
+                    <img src="images/HarleyFrazee.jpg">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Harley Frazee</p>
+                        <p>Email:</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src="images/.JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c1">Heather Fetty</p>
-                    <p>Email:</p>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/.JPG">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Heather Fetty</p>
+                        <p>Email:</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src="images/.JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c1">Katilyn Hepler</p>
-                    <p>Email:</p>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/.JPG">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Katilyn Hepler</p>
+                        <p>Email:</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src="images/.JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c1">Martha</p>
-                    <p>Email:</p>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/.JPG">
+                    <img src="images/Martha.jpg">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Martha</p>
+                        <p>Email:</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src="images/.JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c1">Matt Towey</p>
-                    <p>Email:</p>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/.JPG">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Matt Towey</p>
+                        <p>Email:</p>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="row">
-        <div class="column">
-            <div class="card">
-                <img src="images/DelaneyIrwin.JPG">
-                <div class="container">
-                    <h2>Group Member</h2>
-                    <p class="c1">Delaney Irwin</p>
-                    <p>Email: dmi0003@mix.wvu.edu</p>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/DelaneyIrwin.jpg">
+                    <div class="container">
+                        <h2>Group Member</h2>
+                        <p class="c1">Delaney Irwin</p>
+                        <p>Email: dmi0003@mix.wvu.edu</p>
+                    </div>
                 </div>
             </div>
         </div>
+    </body>
+  
+    <div class="column">
+      <div class="card">
+        <img src="/images/team2.jpg" alt="Martha" style="width:100%">
+        <div class="container">
+          <h2>Martha</h2>
+          <p class="title">Group Member</p>
+          <p>I'm a sophomore majoring in Computer Science and German. In my free time i like hiking and hanging out with friends.</p>
+          <p>mike@example.com</p>
+          </div>
+      </div>
     </div>
-</body>
+    
+     <div class="column">
+      <div class="card">
+        <img src="/images/team2.jpg" alt="Heather" style="width:100%">
+        <div class="container">
+          <h2>Heather Fetty</h2>
+          <p class="title">Group Member</p>
+          <p>Some text that describes me.</p>
+          <p>mike@example.com</p>
+          </div>
+      </div>
+    </div>
+    
+     <div class="column">
+      <div class="card">
+        <img src="/images/team2.jpg" alt="Callyn" style="width:100%">
+        <div class="container">
+          <h2>Callyn Zeigler</h2>
+          <p class="title">Group Member</p>
+          <p>Some text that describes me.</p>
+          <p>mike@example.com</p>
+         </div>
+      </div>
+    </div>
+    
+     <div class="column">
+      <div class="card">
+        <img src="/images/team2.jpg" alt="Katilyn" style="width:100%">
+        <div class="container">
+          <h2>Katilyn Hepler</h2>
+          <p class="title">Group Member</p>
+          <p>Some text that describes me.</p>
+          <p>mike@example.com</p>
+         </div>
+      </div>
+    </div>
+    
+     <div class="column">
+      <div class="card">
+        <img src="/images/team2.jpg" alt="Delaney" style="width:100%">
+        <div class="container">
+          <h2>Delaney Irwin</h2>
+          <p class="title">Group Member</p>
+          <p>Some text that describes me.</p>
+          <p>dmi0003@mix.wvu.edu</p>
+          </div>
+      </div>
+    </div>
+    
+    <div class="column">
+      <div class="card">
+        <img src="/images/team3.jpg" alt="Matt" style="width:100%">
+        <div class="container">
+          <h2>Matt Towey</h2>
+          <p class="title">Group Member</p>
+          <p>Some text that describes me.</p>
+          <p>john@example.com</p>
+          </div>
+      </div>
+    </div>
+    
+    <div class="column">
+      <div class="card">
+        <img src="/images/team3.jpg" alt="Alex" style="width:100%">
+        <div class="container">
+          <h2>Alex Royce</h2>
+          <p class="title">Group Member</p>
+          <p>Some text that describes me.</p>
+          <p>john@example.com</p>
+          </div>
+      </div>
+    </div>
+  </div>
