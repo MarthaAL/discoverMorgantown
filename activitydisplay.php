03506f29a3e96cd8614d74ae66db3ccf325edae8
mtowey99 <70500347+mtowey99@users.noreply.github.com>
2021-05-01 15:59:32 -0400

    Testing Fixes

--- activitydisplay.php
+++ activitydisplay.php
@@ -21,15 +21,16 @@ require 'includes/header.php';
     ?>
 
 
-
-<html>
-<link rel="stylesheet" href="/css/activitydisplay.css">
-
-<title>W3.CSS Template</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+	<title>Activity Display Page</title>
+	<link rel="stylesheet" href="/css/activitydisplay.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+</head>
 
 <main>
 <body class="w3-light-grey">
@@ -54,8 +55,8 @@ require 'includes/header.php';
 
     <!-- Images -->
     <div class="w3-container" >
-      <img class = "img1" src= <?php echo $pic1 ?> >
-      <img class = "img2" src= <?php echo $pic2 ?> >
+      <img class = "img1" src= <?php echo $pic1 ?> alt = "Activity Image 1" >
+      <img class = "img2" src= <?php echo $pic2 ?> alt = "Activity Image 2">
       
       
     </div>
