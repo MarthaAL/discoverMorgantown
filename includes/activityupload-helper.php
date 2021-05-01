1ab84e7d6d7995ddd60ffc0452ed7a68bd28cd65
hpf0004 <hpf0004@mix.wvu.edu>
2021-05-01 08:47:41 -0400

    Fix upload error

--- includes/activityupload-helper.php
+++ includes/activityupload-helper.php
@@ -8,26 +8,26 @@
 
     if (isset($_POST['activity-submit']))
      {
-        $name = $_REQUEST['actname'];
-        $location = $_REQUEST['actlocation'];
-        $description = $_REQUEST["actdescription"];
-        $open = $_REQUEST['open'];
-        $close = $_REQUEST['close'];
-        $tags = $_REQUEST['acttags'];
-        $fakes = $_REQUEST['fakes'];
+        $name = $_POST['actname'];
+        $location = $_POST['actlocation'];
+        $description = $_POST["actdescription"];
+        $open = $_POST['open'];
+        $close = $_POST['close'];
+        $tags = $_POST['acttags'];
+        $fakes = $_POST['fakes'];
         
         $file1 = $_FILES['act-image1'];
         $file2 = $_FILES['act-image2'];
 
-        $file_name1 = $file1['name'];
-        $file_tmp_name1 = $file1['tmp_name'];
-        $file_error1 = $file1['error'];
-        $file_size1 = $file1['size'];
+        $file_name1 = $file1['name1'];
+        $file_tmp_name1 = $file1['tmp_name1'];
+        $file_error1 = $file1['error1'];
+        $file_size1 = $file1['size1'];
 
-        $file_name2 = $file2['name'];
-        $file_tmp_name2 = $file2['tmp_name'];
-        $file_error2 = $file2['error'];
-        $file_size2 = $file2['size'];
+        $file_name2 = $file2['name2'];
+        $file_tmp_name2 = $file2['tmp_name2'];
+        $file_error2 = $file2['error2'];
+        $file_size2 = $file2['size2'];
 
         $ext1 = strtolower(pathinfo($file_name1, PATHINFO_EXTENSION));
         $ext2 = strtolower(pathinfo($file_name2, PATHINFO_EXTENSION));
@@ -59,7 +59,7 @@
             $destination1 = 'activity-images/'.$new_name1;
             $destination2 = 'activity-images/'.$new_name2;
 
-            $ins_query="INSERT INTO activities (`name`,`location`,`description`, `open`, `close`, `tags`, `fakes`, `pic1`, `pic2`) VALUES ('$name','$location','$description','$open', '$close', '$tags', '$fakes', '$destination1', $destination2');";
+            $ins_query="INSERT INTO activities (name, location, description, open, close, tags, fakes, pic1, pic2) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
 
             mysqli_query($conn, $ins_query);
 
