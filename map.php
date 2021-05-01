91f2e5438417c27c262277a4c8bd9ebf71bdf80d
MarthaAL <60011355+MarthaAL@users.noreply.github.com>
2021-05-01 16:12:12 -0400

    Update map.php

--- map.php
+++ map.php
@@ -3,5 +3,5 @@ require 'includes/header.php';
 ?>
 
 <main>
-<iframe src="https://www.google.com/maps/d/embed?mid=1cPhdIEeibgCHlOF2ktulo76MJF_8kKlu&hl=en" width=100% height=100%></iframe>
+<iframe title="map" src="https://www.google.com/maps/d/embed?mid=1cPhdIEeibgCHlOF2ktulo76MJF_8kKlu&hl=en" width=100% height=100%></iframe>
 </main>
