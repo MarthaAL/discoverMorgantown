be0fb5175a98c0be17efbb5ee80b2c57f8f22553
akroyce25 <akr0030@mix.wvu.edu>
2021-05-03 15:49:22 -0400

    fixed footer css

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
-<body>
+<body class="contentforfooter">
 
 <link rel="stylesheet" href="../css/footer.css">
   <footer class="footer">
-    <ul>
+    <ul class="listbox">
         <div class="content">CS 230 Project Group 3</div>
     </ul>
   </footer>
